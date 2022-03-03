0cc38072b6836a51783cc790fea7a3f0d93995aa
Colth4696 <hafer0@gmail.com>
2022-03-02 20:10:34 -0700

    heroku deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ActionCableProvider } from 'react-actioncable-provider';
+import './App.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import {API_WS_ROOT} from './constants';
+// This readys a consumer that will connect against /cable on your backend server by default.
+// const actionCable = {}
+// actionCable.cable = ActionCable.createConsumer(API_WS_ROOT);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <ActionCableProvider url={API_WS_ROOT}>
+         <App />
+    </ActionCableProvider>,
+    document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
